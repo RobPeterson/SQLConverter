88824ee75aea77673bc4ae162c4be0033e172871
Rob Peterson <rpeterson@benefitvision.com>
2019-01-15 12:17:04 -0500

    feat(SQLConverter): add data access.
    Added data access.

---
+++ SQLConverter/DataAccess.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConverter
+{
+  public static class DataAccess
+  {
+    public static string server { get; set; }
+    public static string authentication { get; set; }
+    public static string userName { get; set; }
+    public static string password { get; set; }
+    public static string database { get; set; }
+
+    public static List<string> GetDatabases()
+    {
+      List<string> result = new List<string>();
+      string queryString = "SELECT name FROM sys.databases;";
+      string connectionString = authentication == "Windows Authentication" ? $"Server={server}; Database=master; Integrated Security=True" : $"Server={server}; Database=master; User ID={userName}; Password={password}";
+
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        SqlCommand command = new SqlCommand(queryString, connection);
+        //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+        connection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        try
+        {
+          while (reader.Read())
+          {
+            result.Add(reader["name"].ToString());
+          }
+        }
+        finally
+        {
+          // Always call Close when done reading.
+          reader.Close();
+        }
+      }
+
+      return result;
+    }
+
+    public static List<DatabaseObject> GetStoredProcedures()
+    {
+      List<DatabaseObject> result = new List<DatabaseObject>();
+      string queryString =
+        "SELECT o.name ,m.definition FROM sys.sql_modules AS m INNER JOIN sys.objects AS o ON m.[object_id] = o.[OBJECT_ID] WHERE type = 'p' ORDER BY o.name";
+      string connectionString = GetConnectionString();
+
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        SqlCommand command = new SqlCommand(queryString, connection);
+        //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+        connection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        try
+        {
+          while (reader.Read())
+          {
+            result.Add(new DatabaseObject(){Definition = reader["definition"].ToString(), Name  = reader["name"].ToString()});
+          }
+        }
+        finally
+        {
+          // Always call Close when done reading.
+          reader.Close();
+        }
+      }
+      return result;
+    }
+
+    public static List<DatabaseObject> GetViews()
+    {
+      List<DatabaseObject> result = new List<DatabaseObject>();
+      string queryString = "SELECT o.name ,m.definition FROM sys.sql_modules AS m INNER JOIN sys.objects AS o ON m.[object_id] = o.[OBJECT_ID] WHERE type = 'v' ORDER BY o.name";
+      string connectionString = GetConnectionString();
+
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        SqlCommand command = new SqlCommand(queryString, connection);
+        //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+        connection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        try
+        {
+          while (reader.Read())
+          {
+            result.Add(new DatabaseObject() { Definition = reader["definition"].ToString(), Name = reader["name"].ToString() });
+          }
+        }
+        finally
+        {
+          // Always call Close when done reading.
+          reader.Close();
+        }
+      }
+
+      return result;
+    }
+
+    public static List<DatabaseObject> GetFunctions()
+    {
+      List<DatabaseObject> result = new List<DatabaseObject>();
+      string queryString = "SELECT o.name, m.definition FROM sys.sql_modules AS m INNER JOIN sys.objects AS o ON m.[object_id] = o.[OBJECT_ID] WHERE type IN('IF', 'FS', 'FN') ORDER BY o.Name;";
+      string connectionString = GetConnectionString();
+
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        SqlCommand command = new SqlCommand(queryString, connection);
+        //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+        connection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        try
+        {
+          while (reader.Read())
+          {
+            result.Add(new DatabaseObject() { Definition = reader["definition"].ToString(), Name = reader["name"].ToString() });
+          }
+        }
+        finally
+        {
+          // Always call Close when done reading.
+          reader.Close();
+        }
+      }
+
+      return result;
+    }
+
+
+    private static string GetConnectionString()
+    {
+      var result = authentication == "Windows Authentication" ? $"Server={server}; Database={database}; Integrated Security=True" : $"Server={server}; Database={database}; User ID={userName}; Password={password}";
+      return result;
+    }
+  }
+}
---
+++ SQLConverter/FormSelectDatabase.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SQLConverter
+{
+  partial class FormSelectDatabase
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBoxDatabases = new System.Windows.Forms.ListBox();
+      this.buttonOk = new System.Windows.Forms.Button();
+      this.buttonCancel = new System.Windows.Forms.Button();
+      this.label3 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // listBoxDatabases
+      // 
+      this.listBoxDatabases.FormattingEnabled = true;
+      this.listBoxDatabases.ItemHeight = 16;
+      this.listBoxDatabases.Location = new System.Drawing.Point(11, 32);
+      this.listBoxDatabases.Name = "listBoxDatabases";
+      this.listBoxDatabases.Size = new System.Drawing.Size(295, 196);
+      this.listBoxDatabases.TabIndex = 0;
+      // 
+      // buttonOk
+      // 
+      this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.buttonOk.Location = new System.Drawing.Point(12, 238);
+      this.buttonOk.Name = "buttonOk";
+      this.buttonOk.Size = new System.Drawing.Size(83, 45);
+      this.buttonOk.TabIndex = 1;
+      this.buttonOk.Text = "OK";
+      this.buttonOk.UseVisualStyleBackColor = true;
+      // 
+      // buttonCancel
+      // 
+      this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.buttonCancel.Location = new System.Drawing.Point(216, 238);
+      this.buttonCancel.Name = "buttonCancel";
+      this.buttonCancel.Size = new System.Drawing.Size(91, 45);
+      this.buttonCancel.TabIndex = 2;
+      this.buttonCancel.Text = "Cancel";
+      this.buttonCancel.UseVisualStyleBackColor = true;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(8, 12);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(126, 17);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Select a database:";
+      // 
+      // FormSelectDatabase
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(316, 289);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.buttonCancel);
+      this.Controls.Add(this.buttonOk);
+      this.Controls.Add(this.listBoxDatabases);
+      this.Name = "FormSelectDatabase";
+      this.Text = "Select Database";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox listBoxDatabases;
+    private System.Windows.Forms.Button buttonOk;
+    private System.Windows.Forms.Button buttonCancel;
+    private System.Windows.Forms.Label label3;
+  }
+}
